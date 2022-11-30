c921b1a98af95d59d7fbc9fa6920fef76c16c972
Wallace <dev_kollen@outlook.com>
2022-11-29 22:08:18 -0300

    feat: adicionado funcao de soma

---
+++ math.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Sum(10, 10));
+}
+
+/*
+
+func main() {
+
+}
+
+*/
+func Sum(a int, b int) int {
+	return a + b;
+}
---
+++ math_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	total := Sum(15, 15)
+
+	if total != 30 {
+		t.Errorf("Sum failed");
+	}
+}
